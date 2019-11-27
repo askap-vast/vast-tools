988b6d71739da940583f1694b28b4ecdecc4e08a
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2019-11-27 12:30:49 +1100

    Updated BANE_FOLDER setup

--- vasttools/find_racs.py
+++ vasttools/find_racs.py
@@ -691,34 +691,14 @@ if not BANE_FOLDER:
     if args.use_tiles:
         logger.warning("Background noise estimates are not supported for tiles.")
         logger.warning("Estimating background from mosaics instead.")
-        BANE_FOLDER = '/import/ada1/askap/RACS/aug2019_reprocessing/COMBINED_MOSAICS/I_mosaic_1.0_BANE/'
+    if args.vast_pilot:
+        image_dir = "COMBINED"
+        bane_dir = "STOKES{}_BANE_RMSMAPS".format(stokes_param)
     else:
-        if args.stokesv:
-            BANE_FOLDER = '/import/ada1/askap/RACS/aug2019_reprocessing/COMBINED_MOSAICS/V_mosaic_1.0_BANE/'
-        else:
-            BANE_FOLDER = '/import/ada1/askap/RACS/aug2019_reprocessing/COMBINED_MOSAICS/I_mosaic_1.0_BANE/'
-
-if not os.path.isdir(BANE_FOLDER):
-    logger.critical("{} does not exist. Only finding fields".format(BANE_FOLDER))
-    FIND_FIELDS = True
-
-if not os.path.isdir(SELAVY_FOLDER):
-    logger.critical("{} does not exist. Only finding fields".format(SELAVY_FOLDER))
-    FIND_FIELDS = True
-    
-
-    BANE_FOLDER = args.rms_folder
-if not BANE_FOLDER:
-    if args.use_tiles:
-        logger.warning("Background noise estimates are not supported for tiles.")
-        logger.warning("Estimating background from mosaics instead.")
-        
-        BANE_FOLDER = '/import/ada1/askap/RACS/aug2019_reprocessing/COMBINED_MOSAICS/I_mosaic_1.0_BANE/'
-    else:
-        if args.stokesv:
-            BANE_FOLDER = '/import/ada1/askap/RACS/aug2019_reprocessing/COMBINED_MOSAICS/V_mosaic_1.0_BANE/'
-        else:
-            BANE_FOLDER = '/import/ada1/askap/RACS/aug2019_reprocessing/COMBINED_MOSAICS/I_mosaic_1.0_BANE/'
+        image_dir = "COMBINED_MOSAICS"
+        bane_dir = "{}_mosaic_1.0_BANE".format(stokes_param)
+            
+    BANE_FOLDER = os.path.join(default_base_folder, survey_folder, image_dir, bane_dir)
 
 if not os.path.isdir(BANE_FOLDER):
     logger.critical("{} does not exist. Only finding fields".format(BANE_FOLDER))
