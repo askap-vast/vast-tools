72443f633e249f25f51f935485d91f8558bd4a0c
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2020-01-17 14:15:51 +1100

    third global variable fix

--- vasttools/find_sources.py
+++ vasttools/find_sources.py
@@ -501,7 +501,7 @@ for uf in uniq_fields:
     else:
         fieldname = uf
 
-    image = Image(srcs["sbid"].iloc[0], fieldname, IMAGE_FOLDER, tiles=args.use_tiles)
+    image = Image(srcs["sbid"].iloc[0], fieldname, IMAGE_FOLDER, BANE_FOLDER, tiles=args.use_tiles)
 
     if not args.no_background_rms:
         image.get_rms_img()
@@ -522,7 +522,8 @@ for uf in uniq_fields:
         source = Source(
             fieldname,
             SBID,
-            SELAVY_FOLDER
+            SELAVY_FOLDER,
+            vast_pilot=args.vast_pilot,
             tiles=args.use_tiles,
             stokesv=args.stokesv)
 
