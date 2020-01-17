892ecd1d93a0a36c3afde0dbda22f24dbd9ce96d
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2020-01-17 13:12:27 +1100

    Added init file

---
+++ vasttools/__init__.py
@@ -0,0 +1,12 @@
+'''
+-------------------
+VAST tools module.
+-------------------
+
+This module is a collection of useful scripts, functions and classes for use within the VAST project.
+
+'''
+
+__author__ = 'Dougal Dobie, Adam Stewart'
+__version__ = 1.0.0
+
