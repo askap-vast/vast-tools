1eef6dc3cc51f85675737463c9c182d7cd7d72c6
Adam Stewart <adam.stewart73@gmail.com>
2020-01-22 21:35:58 +1100

    Dropbox download fields mode
    * Ability for the dropbox script to download fields (also using output from find_sources.py --find-fields).
    * Added various filters for the user to apply to easily download what is wanted.
    * Corrected PEP8 in __init__.py

--- bin/get_vast_pilot_dbx.py
+++ bin/get_vast_pilot_dbx.py
@@ -7,6 +7,7 @@ import sys
 import datetime
 import configparser
 import numpy as np
+import pandas as pd
 
 from vasttools.survey import Dropbox
 
@@ -28,6 +29,26 @@ def download_cycle(
         password,
         max_retries,
         main_overwrite):
+    '''
+    Downloads requested files from Dropbox. If a file is not found
+    or corrupted then it retries to a user requested number of
+    times.
+
+    :param files_list: the list of dropbox files to download
+    :type list:
+    :param output_dir: The output directory where the downloads go
+    :type output_dir: str
+    :param shared_url: The Dropbox shared url.
+    :type shared_url: str, optional
+    :param password: Dropbox link password
+    :type password: str
+    :param max_retries: Number of times to attempt re-downloads.
+    :type max_retries: int
+    :param main_overwrite: The user requested overwrite variable
+    :type main_overwrite: bool
+    :returns: list of failed downloads
+    :rtype: list
+    '''
     failures = ["FILLER"]
     retry_count = 0
     complete_failures = []
@@ -58,6 +79,145 @@ def download_cycle(
 
     return complete_failures
 
+def filter_files_list(
+        files_list,
+        fields=[],
+        stokesI_only=False,
+        stokesV_only=False,
+        skip_xml=False,
+        skip_qc=False,
+        skip_islands=False,
+        skip_field_images=False,
+        skip_bkg_images=False,
+        skip_rms_images=False,
+        skip_all_images=False,
+        combined_only=False,
+        tile_only=False):
+    '''
+    Filters the file_list to fetch by the users request.
+
+    :param file_list: the list of dropbox files to filter
+    :type list:
+    :param fields: list of fields to filter, default none.
+    :type fields: list, optional
+    :param stokesI_only: Stokes I only boolean
+    :type stokesI_only: bool, optional
+    :param stokesV_only: Stokes V only boolean
+    :type stokesV_only: bool, optional
+    :param skip_xml: Filter out .xml files
+    :type skip_xml: bool
+    :param skip_qc: Filter out QC files
+    :type skip_qc: bool
+    :param skip_islands: Filter out island selavy files
+    :type skip_islands: bool
+    :param skip_field_images: Filter out field fits files
+    :type skip_field_images: bool
+    :param skip_bkg_images: Filter out bkg fits files
+    :type skip_bkg_images: bool
+    :param skip_rms_images: Filter out rms fits files
+    :type skip_rms_images: bool
+    :param skip_all_images: Filter out .fits files
+    :type skip_all_images: bool
+    :param combined_only: Filter to only combined products
+    :type combined_only: bool
+    :param tile_only: Filter to only tiles products
+    :type tile_only: bool
+    :returns: filtered list of dropbox files
+    :rtype: list
+    '''
+
+    filter_df = pd.DataFrame(data=files_list, columns=["file"])
+
+    if stokesI_only is True and stokesV_only is True:
+        logger.warning(
+            "Stokes I only and Stokes V only are both set to True.")
+        logger.warning("Ignoring.")
+        stokesI_only = False
+        stokesV_only = False
+
+    if combined_only is True and tile_only is True:
+        logger.warning(
+            "Combined only and tiles only are both set to True.")
+        logger.warning("Ignoring.")
+        combined_only = False
+        tiles_only = False
+
+    if len(fields) > 0:
+        field_pattern = "|".join(fields)
+        logger.debug("Filtering fields for {}".format(field_pattern))
+        filter_df = filter_df[filter_df.file.str.contains(field_pattern)]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    if stokesI_only:
+        logger.debug("Filtering to Stokes I only")
+        filter_df = filter_df[filter_df.file.str.contains("STOKESI")]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    if stokesV_only:
+        logger.debug("Filtering to Stokes V only")
+        filter_df = filter_df[filter_df.file.str.contains("STOKESV")]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    if skip_xml:
+        logger.debug("Filtering out XML files.")
+        filter_df = filter_df[~filter_df.file.str.endswith(".xml")]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    if skip_qc:
+        logger.debug("Filtering out QC files.")
+        filter_df = filter_df[~filter_df.file.str.contains("/QC")]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    if skip_islands:
+        logger.debug("Filtering out island files.")
+        filter_df = filter_df[~filter_df.file.str.contains(".islands.")]
+        filter_df.reset_index(drop=True, inplace=True)
+        
+    if not skip_all_images:
+        if skip_field_images:
+            logger.debug("Filtering out field fits files.")
+            pattern = ".I.fits|.V.fits"
+            filter_df = filter_df[~filter_df.file.str.contains(pattern)]
+            filter_df.reset_index(drop=True, inplace=True)
+            
+        if skip_bkg_images:
+            logger.debug("Filtering out rms fits files.")
+            filter_df = filter_df[~filter_df.file.str.endswith("_bkg.fits")]
+            filter_df.reset_index(drop=True, inplace=True)
+            
+        if skip_rms_images:
+            logger.debug("Filtering out rms fits files.")
+            filter_df = filter_df[~filter_df.file.str.endswith("_rms.fits")]
+            filter_df.reset_index(drop=True, inplace=True)
+
+    else:
+        logger.debug("Filtering out fits files.")
+        filter_df = filter_df[~filter_df.file.str.endswith(".fits")]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    if combined_only:
+        logger.debug("Filtering to combined files only.")
+        filter_df = filter_df[filter_df.file.str.contains("/COMBINED/")]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    if tile_only:
+        logger.debug("Filtering to tiles files only.")
+        filter_df = filter_df[filter_df.file.str.contains("/TILES/")]
+        filter_df.reset_index(drop=True, inplace=True)
+        
+    if tile_only:
+        logger.debug("Filtering to tiles files only.")
+        filter_df = filter_df[filter_df.file.str.contains("/TILES/")]
+        filter_df.reset_index(drop=True, inplace=True)
+        
+    if tile_only:
+        logger.debug("Filtering to tiles files only.")
+        filter_df = filter_df[filter_df.file.str.contains("/TILES/")]
+        filter_df.reset_index(drop=True, inplace=True)
+
+    final_list = filter_df.file.tolist()
+
+    return final_list
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -84,6 +244,23 @@ parser.add_argument(
     help='Select to download an entire Epoch directory. Enter as an integer.',
     default=0)
 
+parser.add_argument(
+    '--find-fields-input',
+    type=str,
+    help="Input of fields to fetch (can be obtained from 'find_sources.py').",
+    default=None)
+    
+parser.add_argument(
+    '--find-fields-available-files-input',
+    type=str,
+    help=(
+        "Input already generated list of available files for the download"
+        " fields function to save the script gathering all the files"
+        " available again. I.e. the output of the '--available-files"
+        " option. If not given the script will get the list of files"
+        " from Dropbox."),
+    default=None)
+    
 parser.add_argument(
     '--files-list',
     type=str,
@@ -129,6 +306,61 @@ parser.add_argument(
     type=int,
     help='How many times to attempt to retry a failed download',
     default=2)
+
+parser.add_argument(
+    '--stokesI-only',
+    action="store_true",
+    help="Only download STOKES I products.")
+
+parser.add_argument(
+    '--stokesV-only',
+    action="store_true",
+    help="Only download STOKES V products.")
+
+parser.add_argument(
+    '--skip-xml',
+    action="store_true",
+    help="Do not download XML files.")
+    
+parser.add_argument(
+    '--skip-qc',
+    action="store_true",
+    help="Do not download the QC plots.")
+
+parser.add_argument(
+    '--skip-islands',
+    action="store_true",
+    help="Only download component selavy files.")
+
+parser.add_argument(
+    '--skip-field-images',
+    action="store_true",
+    help="Do not download field images.")
+
+parser.add_argument(
+    '--skip-bkg-images',
+    action="store_true",
+    help="Do not download background images.")
+
+parser.add_argument(
+    '--skip-rms-images',
+    action="store_true",
+    help="Do not download background images.")
+
+parser.add_argument(
+    '--skip-all-images',
+    action="store_true",
+    help="Only download non-image data products.")
+
+parser.add_argument(
+    '--combined-only',
+    action="store_true",
+    help="Only download the combined products.")
+    
+parser.add_argument(
+    '--tile-only',
+    action="store_true",
+    help="Only download the combined products.")
 
 args = parser.parse_args()
 
@@ -276,9 +508,27 @@ elif args.download_epoch != 0:
 
         for folder in folders_list:
             os.makedirs(os.path.join(output_dir, folder[1:]), exist_ok=True)
-        logger.info("Downloading files for {}...".format(epoch_string))
+
+        files_to_download = filter_files_list(
+                                files_list,
+                                stokesI_only=args.stokesI_only,
+                                stokesV_only=args.stokesV_only,
+                                skip_xml=args.skip_xml,
+                                skip_qc=args.skip_qc,
+                                skip_islands=args.skip_islands,
+                                skip_field_images=args.skip_field_images,
+                                skip_bkg_images=args.skip_bkg_images,
+                                skip_rms_images=args.skip_rms_images,
+                                skip_all_images=args.skip_all_images,
+                                combined_only=args.combined_only,
+                                tile_only=args.tile_only
+                                )
+
+        logger.info(
+            "Downloading {} files for {}...".format(
+                len(files_to_download), epoch_string))
         complete_failures = download_cycle(
-            files_list,
+            files_to_download,
             output_dir,
             shared_url,
             password,
@@ -290,6 +540,73 @@ elif args.download_epoch != 0:
                 logger.warning(fail)
             logger.warning("These files may be corrupted!")
 
+elif args.find_fields_input is not None:
+    if not os.path.isfile(args.find_fields_input):
+        logger.error(
+            "Supplied file '{}' not found!".format(args.find_fields_input))
+        sys.exit()
+    fields_df = pd.read_csv(args.find_fields_input)
+    fields_to_fetch = fields_df.field_name.unique()
+    logger.info("Will download data products of the following fields:")
+    for f in fields_to_fetch:
+        logger.info(f)
+    if args.find_fields_available_files_input is not None:
+        if not os.path.isfile(args.find_fields_available_files_input):
+            logger.error(
+                "Available files list file {} "
+                "not found!".format(args.find_fields_available_files_input))
+            logger.error(
+                "Check file or remove the"
+                " '--find-fields-available-files-list' option.")
+            sys.exit()
+        with open(args.find_fields_available_files_input, 'r') as f:
+            lines = f.readlines()
+        files_list = [i.strip() for i in lines]
+    else:
+        # We need to search all files so we need to make the main search
+        files_list, folders_list = vast_dropbox.recursive_build_files(
+            base_file_list,
+            legacy=args.include_legacy)
+    files_to_download = filter_files_list(
+                            files_list,
+                            fields=fields_to_fetch,
+                            stokesI_only=args.stokesI_only,
+                            stokesV_only=args.stokesV_only,
+                            skip_xml=args.skip_xml,
+                            skip_qc=args.skip_qc,
+                            skip_islands=args.skip_islands,
+                            skip_field_images=args.skip_field_images,
+                            skip_bkg_images=args.skip_bkg_images,
+                            skip_rms_images=args.skip_rms_images,
+                            skip_all_images=args.skip_all_images,
+                            combined_only=args.combined_only,
+                            tile_only=args.tile_only
+                            )
+
+    dirs_to_create = np.unique(
+        ["/".join(i.strip().split("/")[1:-1]) for i in files_to_download])
+
+    for i in dirs_to_create:
+        if i == "":
+            continue
+        os.makedirs(os.path.join(output_dir, i), exist_ok=True)                        
+    
+    logger.info(
+        "Downloading {} files for {} fields...".format(
+            len(files_to_download), len(fields_to_fetch)))
+    complete_failures = download_cycle(
+        files_to_download,
+        output_dir,
+        shared_url,
+        password,
+        args.max_retries,
+        args.overwrite)
+    if len(complete_failures) > 0:
+        logger.warning("The following files failed to download correctly:")
+        for fail in complete_failures:
+            logger.warning(fail)
+        logger.warning("These files may be corrupted!")
+        
 elif args.files_list is not None:
     if not os.path.isfile(args.files_list):
         logger.error("Supplied file '{}' not found!".format(args.files_list))
@@ -298,16 +615,31 @@ elif args.files_list is not None:
         userlines = f.readlines()
 
     # check files start with / and ignore #
-    files_to_download = []
+    files_list = []
 
     for i in userlines:
         if i.startswith("#"):
             continue
         else:
             if i.startswith("/"):
-                files_to_download.append(i.strip())
+                files_list.append(i.strip())
             else:
-                files_to_download.append("/{}".format(i.strip()))
+                files_list.append("/{}".format(i.strip()))
+
+    files_to_download = filter_files_list(
+                            files_list,
+                            stokesI_only=args.stokesI_only,
+                            stokesV_only=args.stokesV_only,
+                            skip_xml=args.skip_xml,
+                            skip_qc=args.skip_qc,
+                            skip_islands=args.skip_islands,
+                            skip_field_images=args.skip_field_images,
+                            skip_bkg_images=args.skip_bkg_images,
+                            skip_rms_images=args.skip_rms_images,
+                            skip_all_images=args.skip_all_images,
+                            combined_only=args.combined_only,
+                            tile_only=args.tile_only
+                            )
 
     dirs_to_create = np.unique(
         ["/".join(i.strip().split("/")[1:-1]) for i in files_to_download])
--- vasttools/__init__.py
+++ vasttools/__init__.py
@@ -3,10 +3,10 @@
 VAST tools module.
 -------------------
 
-This module is a collection of useful scripts, functions and classes for use within the VAST project.
+This module is a collection of useful scripts, 
+functions and classes for use within the VAST project.
 
 '''
 
 __author__ = 'Dougal Dobie, Adam Stewart'
 __version__ = "0.4.0"
-
