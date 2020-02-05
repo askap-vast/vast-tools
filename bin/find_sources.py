2850fa928f711dc5008f2998df6a79477eea7e11
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2020-02-05 11:00:43 +1100

    Built first 3 functions

--- bin/find_sources.py
+++ bin/find_sources.py
@@ -63,222 +63,270 @@ HOST_ADA = 'ada.physics.usyd.edu.au'
 
 runstart = datetime.datetime.now()
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument(
-    'coords',
-    metavar="\"HH:MM:SS [+/-]DD:MM:SS\" OR input.csv",
-    type=str,
-    help=("Right Ascension and Declination in format "
-          "\"HH:MM:SS [+/-]DD:MM:SS\", in quotes. "
-          "E.g. \"12:00:00 -20:00:00\". Degrees is also acceptable, "
-          "e.g. \"12.123 -20.123\". Multiple coordinates are supported "
-          "by separating with a comma (no space) e.g. "
-          "\"12.231 -56.56,123.4 +21.3\"."
-          " Finally you can also enter coordinates using a .csv file."
-          " See example file for format."))
+def parse_args():
+    '''
+    Parse arguments
+    
+    :returns: Argument namespace
+    :rtype: `argparse.Namespace`
+    '''
+    
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument(
-    '--vast-pilot',
-    choices=["0", ]+sorted(RELEASED_EPOCHS),
-    help=("Select the VAST Pilot Epoch to query. "
-          "Epoch 0 is RACS."),
-    default="1")
-parser.add_argument(
-    '--imsize',
-    type=float,
-    help='Edge size of the postagestamp in arcmin',
-    default=30.)
-parser.add_argument(
-    '--maxsep',
-    type=float,
-    help='Maximum separation of source from beam centre in degrees.',
-    default=1.0)
-parser.add_argument(
-    '--out-folder',
-    type=str,
-    help='Name of the output directory to place all results in.',
-    default="find_sources_output_{}".format(
-        runstart.strftime("%Y%m%d_%H:%M:%S")))
-parser.add_argument(
-    '--source-names',
-    type=str,
-    help=("Only for use when entering coordaintes via the command line. "
-          "State the name of the source being searched. "
-          "Use quote marks for names that contain a space. "
-          "For multiple sources separate with a comma with no "
-          "space, e.g. \"SN 1994N,SN 2003D,SN 2019A\"."),
-    default="")
-parser.add_argument(
-    '--crossmatch-radius',
-    type=float,
-    help='Crossmatch radius in arcseconds',
-    default=15.0)
-parser.add_argument(
-    '--crossmatch-radius-overlay',
-    action="store_true",
-    help=('A circle is placed on all PNG and region/annotation'
-          ' files to represent the crossmatch radius.'))
-parser.add_argument(
-    '--use-tiles',
-    action="store_true",
-    help='Use the individual tiles instead of combined mosaics.')
-parser.add_argument(
-    '--base-folder',
-    type=str,
-    help='Path to base folder if using default directory structure')
-parser.add_argument(
-    '--img-folder',
-    type=str,
-    help='Path to folder where images are stored')
-parser.add_argument(
-    '--rms-folder',
-    type=str,
-    help='Path to folder where image RMS estimates are stored')
-parser.add_argument(
-    '--cat-folder',
-    type=str,
-    help='Path to folder where selavy catalogues are stored')
-parser.add_argument(
-    '--create-png',
-    action="store_true",
-    help='Create a png of the fits cutout.')
-parser.add_argument(
-    '--png-selavy-overlay',
-    action="store_true",
-    help='Overlay selavy components onto the png image.')
-parser.add_argument(
-    '--png-linear-percentile',
-    type=float,
-    default=99.9,
-    help='Choose the percentile level for the png normalisation.')
-parser.add_argument(
-    '--png-use-zscale',
-    action="store_true",
-    help='Select ZScale normalisation (default is \'linear\').')
-parser.add_argument(
-    '--png-zscale-contrast',
-    type=float,
-    default=0.1,
-    help='Select contrast to use for zscale.')
-parser.add_argument(
-    '--png-hide-beam',
-    action="store_true",
-    help='Select to not show the image synthesised beam on the plot.')
-parser.add_argument(
-    '--png-no-island-labels',
-    action="store_true",
-    help='Disable island lables on the png.')
-parser.add_argument(
-    '--png-no-colorbar',
-    action="store_true",
-    help='Do not show the colorbar on the png.')
-parser.add_argument(
-    '--ann',
-    action="store_true",
-    help='Create a kvis annotation file of the components.')
-parser.add_argument(
-    '--reg',
-    action="store_true",
-    help='Create a DS9 region file of the components.')
-parser.add_argument(
-    '--stokesv',
-    action="store_true",
-    help='Use Stokes V images and catalogues if available.')
-parser.add_argument(
-    '--quiet',
-    action="store_true",
-    help='Turn off non-essential terminal output.')
-parser.add_argument(
-    '--crossmatch-only',
-    action="store_true",
-    help='Only run crossmatch, do not generate any fits or png files.')
-parser.add_argument(
-    '--selavy-simple',
-    action="store_true",
-    help='Only include flux density and uncertainty in returned table.')
-parser.add_argument(
-    '--process-matches',
-    action="store_true",
-    help='Only produce data products for sources that have a selavy match.')
-parser.add_argument(
-    '--debug',
-    action="store_true",
-    help='Turn on debug output.')
-parser.add_argument(
-    '--no-background-rms',
-    action="store_true",
-    help='Do not estimate the background RMS around each source.')
-parser.add_argument(
-    '--find-fields',
-    action="store_true",
-    help='Only return the associated field for each source.')
-parser.add_argument(
-    '--clobber',
-    action="store_true",
-    help=("Overwrite the output directory if it already exists."))
+    parser.add_argument(
+        'coords',
+        metavar="\"HH:MM:SS [+/-]DD:MM:SS\" OR input.csv",
+        type=str,
+        help=("Right Ascension and Declination in format "
+              "\"HH:MM:SS [+/-]DD:MM:SS\", in quotes. "
+              "E.g. \"12:00:00 -20:00:00\". Degrees is also acceptable, "
+              "e.g. \"12.123 -20.123\". Multiple coordinates are supported "
+              "by separating with a comma (no space) e.g. "
+              "\"12.231 -56.56,123.4 +21.3\"."
+              " Finally you can also enter coordinates using a .csv file."
+              " See example file for format."))
 
-args = parser.parse_args()
+    parser.add_argument(
+        '--vast-pilot',
+        choices=["0", ]+sorted(RELEASED_EPOCHS),
+        help=("Select the VAST Pilot Epoch to query. "
+              "Epoch 0 is RACS."),
+        default="1")
+    parser.add_argument(
+        '--imsize',
+        type=float,
+        help='Edge size of the postagestamp in arcmin',
+        default=30.)
+    parser.add_argument(
+        '--maxsep',
+        type=float,
+        help='Maximum separation of source from beam centre in degrees.',
+        default=1.0)
+    parser.add_argument(
+        '--out-folder',
+        type=str,
+        help='Name of the output directory to place all results in.',
+        default="find_sources_output_{}".format(
+            runstart.strftime("%Y%m%d_%H:%M:%S")))
+    parser.add_argument(
+        '--source-names',
+        type=str,
+        help=("Only for use when entering coordaintes via the command line. "
+              "State the name of the source being searched. "
+              "Use quote marks for names that contain a space. "
+              "For multiple sources separate with a comma with no "
+              "space, e.g. \"SN 1994N,SN 2003D,SN 2019A\"."),
+        default="")
+    parser.add_argument(
+        '--crossmatch-radius',
+        type=float,
+        help='Crossmatch radius in arcseconds',
+        default=15.0)
+    parser.add_argument(
+        '--crossmatch-radius-overlay',
+        action="store_true",
+        help=('A circle is placed on all PNG and region/annotation'
+              ' files to represent the crossmatch radius.'))
+    parser.add_argument(
+        '--use-tiles',
+        action="store_true",
+        help='Use the individual tiles instead of combined mosaics.')
+    parser.add_argument(
+        '--base-folder',
+        type=str,
+        help='Path to base folder if using default directory structure')
+    parser.add_argument(
+        '--img-folder',
+        type=str,
+        help='Path to folder where images are stored')
+    parser.add_argument(
+        '--rms-folder',
+        type=str,
+        help='Path to folder where image RMS estimates are stored')
+    parser.add_argument(
+        '--cat-folder',
+        type=str,
+        help='Path to folder where selavy catalogues are stored')
+    parser.add_argument(
+        '--create-png',
+        action="store_true",
+        help='Create a png of the fits cutout.')
+    parser.add_argument(
+        '--png-selavy-overlay',
+        action="store_true",
+        help='Overlay selavy components onto the png image.')
+    parser.add_argument(
+        '--png-linear-percentile',
+        type=float,
+        default=99.9,
+        help='Choose the percentile level for the png normalisation.')
+    parser.add_argument(
+        '--png-use-zscale',
+        action="store_true",
+        help='Select ZScale normalisation (default is \'linear\').')
+    parser.add_argument(
+        '--png-zscale-contrast',
+        type=float,
+        default=0.1,
+        help='Select contrast to use for zscale.')
+    parser.add_argument(
+        '--png-hide-beam',
+        action="store_true",
+        help='Select to not show the image synthesised beam on the plot.')
+    parser.add_argument(
+        '--png-no-island-labels',
+        action="store_true",
+        help='Disable island lables on the png.')
+    parser.add_argument(
+        '--png-no-colorbar',
+        action="store_true",
+        help='Do not show the colorbar on the png.')
+    parser.add_argument(
+        '--ann',
+        action="store_true",
+        help='Create a kvis annotation file of the components.')
+    parser.add_argument(
+        '--reg',
+        action="store_true",
+        help='Create a DS9 region file of the components.')
+    parser.add_argument(
+        '--stokesv',
+        action="store_true",
+        help='Use Stokes V images and catalogues if available.')
+    parser.add_argument(
+        '--quiet',
+        action="store_true",
+        help='Turn off non-essential terminal output.')
+    parser.add_argument(
+        '--crossmatch-only',
+        action="store_true",
+        help='Only run crossmatch, do not generate any fits or png files.')
+    parser.add_argument(
+        '--selavy-simple',
+        action="store_true",
+        help='Only include flux density and uncertainty in returned table.')
+    parser.add_argument(
+        '--process-matches',
+        action="store_true",
+        help='Only produce data products for sources that have a selavy match.')
+    parser.add_argument(
+        '--debug',
+        action="store_true",
+        help='Turn on debug output.')
+    parser.add_argument(
+        '--no-background-rms',
+        action="store_true",
+        help='Do not estimate the background RMS around each source.')
+    parser.add_argument(
+        '--find-fields',
+        action="store_true",
+        help='Only return the associated field for each source.')
+    parser.add_argument(
+        '--clobber',
+        action="store_true",
+        help=("Overwrite the output directory if it already exists."))
 
-logger = logging.getLogger()
-s = logging.StreamHandler()
-fh = logging.FileHandler(
-    "find_sources_{}.log".format(
-        runstart.strftime("%Y%m%d_%H:%M:%S")))
-fh.setLevel(logging.DEBUG)
-logformat = '[%(asctime)s] - %(levelname)s - %(message)s'
+    args = parser.parse_args()
+    
+    return args
 
-if use_colorlog:
-    formatter = colorlog.ColoredFormatter(
-        "%(log_color)s[%(asctime)s] - %(levelname)s - %(blue)s%(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        reset=True,
-        log_colors={
-            'DEBUG': 'cyan',
-            'INFO': 'green',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': 'red,bg_white', },
-        secondary_log_colors={},
-        style='%'
-    )
-else:
-    formatter = logging.Formatter(logformat, datefmt="%Y-%m-%d %H:%M:%S")
+def get_logger(args, use_colorlog=False):
+    '''
+    Set up the logger
+    
+    :param args: Arguments namespace
+    :type args: `argparse.Namespace`
+    :param usecolorlog: Use colourful logging scheme, defaults to False
+    :type usecolorlog: bool, optional
+    
+    :returns: Logger
+    :rtype: `logging.RootLogger`
+    '''
+    
+    logger = logging.getLogger()
+    s = logging.StreamHandler()
+    fh = logging.FileHandler(
+        "find_sources_{}.log".format(
+            runstart.strftime("%Y%m%d_%H:%M:%S")))
+    fh.setLevel(logging.DEBUG)
+    logformat = '[%(asctime)s] - %(levelname)s - %(message)s'
 
-s.setFormatter(formatter)
-fh.setFormatter(formatter)
-
-if args.debug:
-    s.setLevel(logging.DEBUG)
-else:
-    if args.quiet:
-        s.setLevel(logging.WARNING)
+    if use_colorlog:
+        formatter = colorlog.ColoredFormatter(
+            "%(log_color)s[%(asctime)s] - %(levelname)s - %(blue)s%(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            reset=True,
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red,bg_white', },
+            secondary_log_colors={},
+            style='%'
+        )
     else:
-        s.setLevel(logging.INFO)
+        formatter = logging.Formatter(logformat, datefmt="%Y-%m-%d %H:%M:%S")
 
-logger.addHandler(s)
-logger.addHandler(fh)
-logger.setLevel(logging.DEBUG)
+    s.setFormatter(formatter)
+    fh.setFormatter(formatter)
 
-# Sort out output directory
-output_name = args.out_folder
-if os.path.isdir(output_name):
-    if args.clobber:
-        logger.warning(("Directory {} already exists "
-                        "but clobber selected. "
-                        "Removing current directory."
-                        ).format(output_name))
-        shutil.rmtree(output_name)
+    if args.debug:
+        s.setLevel(logging.DEBUG)
     else:
-        logger.critical(
-            ("Requested output directory '{}' already exists! "
-             "Will not overwrite.").format(output_name))
-        logger.critical("Exiting.")
-        sys.exit()
+        if args.quiet:
+            s.setLevel(logging.WARNING)
+        else:
+            s.setLevel(logging.INFO)
 
-logger.info("Creating directory '{}'.".format(output_name))
-os.mkdir(output_name)
+    logger.addHandler(s)
+    logger.addHandler(fh)
+    logger.setLevel(logging.DEBUG)
+    
+    return logger
+
+
+def get_output_directory(args):
+    '''
+    Build the output directory
+    
+    :param args: Arguments namespace
+    :type args: `argparse.Namespace`
+    
+    :returns: Output directory
+    :rtype: str
+    '''
+    
+    output_name = args.out_folder
+    if os.path.isdir(output_name):
+        if args.clobber:
+            logger.warning(("Directory {} already exists "
+                            "but clobber selected. "
+                            "Removing current directory."
+                            ).format(output_name))
+            shutil.rmtree(output_name)
+        else:
+            logger.critical(
+                ("Requested output directory '{}' already exists! "
+                 "Will not overwrite.").format(output_name))
+            logger.critical("Exiting.")
+            sys.exit()
+
+    logger.info("Creating directory '{}'.".format(output_name))
+    os.mkdir(output_name)
+    
+    return output_name
+
+args = parse_args()
+logger = get_logger(args, use_colorlog=use_colorlog)
+print(type(logger))
+exit()
+
+
+
+
 
 if " " not in args.coords:
     logger.info("Loading file {}".format(args.coords))
