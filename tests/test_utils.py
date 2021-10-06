86f7a9789543cebff63d591b4f975f1d69681820
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2021-10-06 14:59:07 +1100

    Initial testing attempt

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,7 @@ from pytest_mock import mocker
 
 import vasttools.utils as vtu
 
+TEST_DATA_DIR = Path(__file__).resolve().parent / 'data'
 
 @pytest.fixture
 def coords_df() -> pd.DataFrame:
@@ -186,6 +187,22 @@ def source_df() -> pd.DataFrame:
     """
     source_df = pd.DataFrame(
         data={
+            'ra': [
+                12.59305887,
+                12.68310734,
+                0.0,
+                345.0,
+                30.0,
+                9.538331008
+            ],
+            'dec': [
+                -23.06359886,
+                -24.26722689,
+                0.0,
+                -46.12,
+                -2.3,
+                -23.29754621
+            ],
             'flux_peak': [
                 -0.1536501944065094,
                 -0.11119924485683441,
@@ -668,3 +685,48 @@ def test_pipeline_get_variable_metrics_zero(source_df: pd.DataFrame) -> None:
     )
 
     assert np.all(results.to_numpy() == 0.0)
+    
+
+def test_find_in_moc(source_df: pd.DataFrame) -> None:
+    """
+    Tests finding which sources are contained within a MOC.
+    
+    Args: 
+        source_df: The dataframe containing the source data.
+    
+    Returns:
+        None
+    """
+    filename = TEST_DATA_DIR / 'test_skymap_gw190814.fits.gz'
+    results = [True, False, False, False, False, True]
+    
+    moc_skymap = vtu.skymap2moc(filename, 0.8)
+    
+    func_output = vtu.find_in_moc(moc_skymap, source_df, pipe=False)
+    
+    assert func_output == results
+    
+
+def test_add_credible_levels(source_df: pd.DataFrame) -> None:
+    """
+    Tests adding the credible levels from a skymap.
+    
+    Args:
+        source_df: The dataframe containing the source data.
+    
+    Returns:
+        None
+    """
+    credible_levels = [0.802502,
+                       0.28886,
+                       1,
+                       1,
+                       1,
+                       0.978301
+                      ]
+
+    filename = TEST_DATA_DIR / 'test_skymap_gw190814.fits.gz'
+    vtu.add_credible_levels(filename, source_df, pipe=False)
+    
+    assert source_df['credible_level'].values == credible_levels
+
