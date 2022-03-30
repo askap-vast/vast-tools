9b239c6fcd21c4447d28c2e2d97362cc4c92daae
Dougal Dobie <ddobie@swin.edu.au>
2022-03-30 12:40:12 +1100

    Remove epoch 12 user warning

--- vasttools/utils.py
+++ vasttools/utils.py
@@ -621,29 +621,6 @@ def calculate_m_metric(flux_a: float, flux_b: float) -> float:
     return 2 * ((flux_a - flux_b) / (flux_a + flux_b))
 
 
-def epoch12_user_warning() -> None:
-    """
-    A function to raise a user warning about the new epoch 12 and 13
-    definitions.
-
-    To be removed in a future release.
-
-    Returns:
-        None
-    """
-    # TODO: Remove warning in future release.
-    warning_msg = (
-        "Using v2.0.0 epoch definitions which inserts a new epoch 12, "
-        "displacing the existing epoch 12 to epoch 13. "
-        "Code written before this time that uses vast-tools may need to be "
-        "updated to reproduce results. See "
-        "https://github.com/askap-vast/vast-project/wiki/"
-        "Pilot-Survey-Status-&-Data"
-    )
-
-    warnings.warn(warning_msg)
-
-
 def _distance_from_edge(x: np.ndarray) -> np.ndarray:
     """
     Analyses the binary array x and determines the distance from
